4b1e586a8329d8449305f089dbf58870b9fed52e
namanydv <namanofficial91@gmail.com>
2026-01-06 23:20:23 +0530

    add autoplay option

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,6 +38,11 @@ const swiperProjects = new Swiper('.projects__swiper', {
   pagination: {
     el: '.swiper-pagination',
   },
+
+  autoplay:{
+    delay:3000,
+    disableOnInteraction: false
+  }
 });
 
 
